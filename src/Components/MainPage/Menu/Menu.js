edb72a08fad8fbd9ff8dddf41ca6f284c26f1ba3
SurCC <sursahakyan@gmail.com>
2022-10-22 13:32:54 +0400

    Finished the main stracture of main page

--- src/Components/MainPage/Menu/Menu.js
+++ src/Components/MainPage/Menu/Menu.js
@@ -1,8 +1,88 @@
 import React from "react";
-import "./Menu.css"
+import "./Menu.css";
+import Data from "./data";
+import { Link } from "react-router-dom";
 
 export default function Menu() {
-    return (
-        <div className="menu"></div>
-    )
+  return (
+    <div className="menu">
+      <div className="insideData">
+        {Data.map((insideData) => (
+          <div className="containerOfMenu" key={insideData.index}>
+            <div
+              className={
+                (insideData.index < 2 && "insideDataHeader") ||
+                (insideData.index > 2 && "insideDataHeader3") ||
+                "insideDataHeader2"
+              }
+            >
+              {insideData.name}
+            </div>
+            {insideData.topSales && (
+              <div className="topSales">{insideData.topSales}</div>
+            )}
+            <div className="firstRowOfInsideData">
+              {insideData.list.map(
+                (insideDataMenu) =>
+                  insideDataMenu.index <= 6 && (
+                    <div key={insideDataMenu.index} className="slideOfitem">
+                      <img
+                        src={insideDataMenu.imgSrc}
+                        alt="insideDataMenuImage"
+                      ></img>
+                      <div
+                        className={
+                          insideDataMenu.index % 2 !== 0
+                            ? `containerOfNameSizeAndPrice`
+                            : `containerOfNameSizeAndPriceTop`
+                        }
+                      >
+                        <div className="nameOfItem">{insideDataMenu.name}</div>
+                        <div className="sizeAndPrice">
+                          <div className="sizeOfItem">
+                            {insideDataMenu.size}
+                          </div>
+                          <div className="priceOfItem">
+                            {insideDataMenu.price}
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  )
+              )}
+            </div>
+            <div className="secondRowOfInsideData">
+              {insideData.list.map(
+                (insideDataMenu) =>
+                  insideDataMenu.index > 6 && (
+                    <div key={insideDataMenu.index} className="slideOfitem">
+                      <img
+                        src={insideDataMenu.imgSrc}
+                        alt="insideDataMenuImage"
+                      ></img>
+                      <div className="containerOfNameSizeAndPrice">
+                        <div className="nameOfItem">{insideDataMenu.name}</div>
+                        <div className="sizeAndPrice">
+                          <div className="sizeOfItem">
+                            {insideDataMenu.size}
+                          </div>
+                          <div className="priceOfItem">
+                            {insideDataMenu.price}
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  )
+              )}
+            </div>
+            <div className="moreOfBarelief">
+              <Link to="./more">
+                <button className="moreButtonOfbarelief">Узнать больше</button>
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
---
+++ src/Components/MainPage/Menu/data.js
@@ -0,0 +1,96 @@
+import Image1 from "../Images/Bareliefs/M-1.png";
+import Image2 from "../Images/Bareliefs/M-2.png";
+import Image3 from "../Images/Bareliefs/M-3.png";
+import Image4 from "../Images/Bareliefs/M-4.png";
+import Image5 from "../Images/Bareliefs/M-5.png";
+import Image6 from "../Images/Bareliefs/M-6.png";
+import Image7 from "../Images/Bareliefs/M-7.png";
+import Image8 from "../Images/Bareliefs/M-8.png";
+import Image9 from "../Images/Bareliefs/M-9.png";
+import Image10 from "../Images/Bareliefs/M-10.png";
+import Image11 from "../Images/Bareliefs/M-11.png";
+import Image12 from "../Images/Bareliefs/M-12.png";
+import Image13 from "../Images/Printography/M-13.png";
+import Image14 from "../Images/Printography/M-14.png";
+import Image15 from "../Images/Printography/M-15.png";
+import Image16 from "../Images/Printography/M-16.png";
+import Image17 from "../Images/Printography/M-17.png";
+import Image18 from "../Images/Printography/M-18.png";
+import Image19 from "../Images/Printography/M-19.png";
+import Image20 from "../Images/Printography/M-20.png";
+import Image21 from "../Images/Printography/M-21.png";
+import Image22 from "../Images/Printography/M-22.png";
+import Image23 from "../Images/Printography/M-23.png";
+import Image24 from "../Images/Printography/M-24.png";
+import Image25 from "../Images/3D gypsum panels/M-25.png";
+import Image26 from "../Images/3D gypsum panels/M-26.png";
+import Image27 from "../Images/3D gypsum panels/M-27.png";
+import Image28 from "../Images/3D gypsum panels/M-28.png";
+import Image29 from "../Images/3D gypsum panels/M-29.png";
+import Image30 from "../Images/3D gypsum panels/M-30.png";
+import Image31 from "../Images/3D gypsum panels/M-31.png";
+import Image32 from "../Images/3D gypsum panels/M-32.png";
+import Image33 from "../Images/3D gypsum panels/M-33.png";
+import Image34 from "../Images/3D gypsum panels/M-34.png";
+import Image35 from "../Images/3D gypsum panels/M-35.png";
+import Image36 from "../Images/3D gypsum panels/M-36.png";
+
+const data = [
+  {
+    name: "Барельефы",
+    index: 1,
+    topSales: "Лидеры продаж",
+    list: [
+      { name: "M-1", index: 1, size: "165x143x22 мм", imgSrc: Image1, price: "83.99₽/шт" },
+      { name: "M-2", index: 2, size: "165x143x22 мм", imgSrc: Image2, price: "83.99₽/шт" },
+      { name: "M-3", index: 3, size: "165x143x22 мм", imgSrc: Image3, price: "83.99₽/шт" },
+      { name: "M-4", index: 4, size: "165x143x22 мм", imgSrc: Image4, price: "83.99₽/шт" },
+      { name: "M-5", index: 5, size: "165x143x22 мм", imgSrc: Image5, price: "83.99₽/шт" },
+      { name: "M-6", index: 6, size: "165x143x22 мм", imgSrc: Image6, price: "83.99₽/шт" },
+      { name: "M-7", index: 7, size: "165x143x22 мм", imgSrc: Image7, price: "83.99₽/шт" },
+      { name: "M-8", index: 8, size: "165x143x22 мм", imgSrc: Image8, price: "83.99₽/шт" },
+      { name: "M-9", index: 9, size: "165x143x22 мм", imgSrc: Image9, price: "83.99₽/шт" },
+      { name: "M-10", index: 10, size: "165x143x22 мм", imgSrc: Image10, price: "83.99₽/шт" },
+      { name: "M-11", index: 11, size: "165x143x22 мм", imgSrc: Image11, price: "83.99₽/шт" },
+      { name: "M-12", index: 12, size: "165x143x22 мм", imgSrc: Image12, price: "83.99₽/шт" },
+    ],
+  },
+  {
+    name: "Принтография",
+    index: 2,
+    list: [
+      { name: "M-13", index: 1, size: "165x143x22 мм", imgSrc: Image13, price: "83.99₽/шт" },
+      { name: "M-14", index: 2, size: "165x143x22 мм", imgSrc: Image14, price: "83.99₽/шт" },
+      { name: "M-15", index: 3, size: "165x143x22 мм", imgSrc: Image15, price: "83.99₽/шт" },
+      { name: "M-16", index: 4, size: "165x143x22 мм", imgSrc: Image16, price: "83.99₽/шт" },
+      { name: "M-17", index: 5, size: "165x143x22 мм", imgSrc: Image17, price: "83.99₽/шт" },
+      { name: "M-18", index: 6, size: "165x143x22 мм", imgSrc: Image18, price: "83.99₽/шт" },
+      { name: "M-19", index: 7, size: "165x143x22 мм", imgSrc: Image19, price: "83.99₽/шт" },
+      { name: "M-20", index: 8, size: "165x143x22 мм", imgSrc: Image20, price: "83.99₽/шт" },
+      { name: "M-21", index: 9, size: "165x143x22 мм", imgSrc: Image21, price: "83.99₽/шт" },
+      { name: "M-22", index: 10, size: "165x143x22 мм", imgSrc: Image22, price: "83.99₽/шт" },
+      { name: "M-23", index: 11, size: "165x143x22 мм", imgSrc: Image23, price: "83.99₽/шт" },
+      { name: "M-24", index: 12, size: "165x143x22 мм", imgSrc: Image24, price: "83.99₽/шт" },
+    ],
+  },
+  {
+    name: "3D гипсовые панели",
+    index: 3,
+    list: [
+      { name: "M-25", index: 1, size: "165x143x22 мм", imgSrc: Image25, price: "83.99₽/шт" },
+      { name: "M-26", index: 2, size: "165x143x22 мм", imgSrc: Image26, price: "83.99₽/шт" },
+      { name: "M-27", index: 3, size: "165x143x22 мм", imgSrc: Image27, price: "83.99₽/шт" },
+      { name: "M-28", index: 4, size: "165x143x22 мм", imgSrc: Image28, price: "83.99₽/шт" },
+      { name: "M-29", index: 5, size: "165x143x22 мм", imgSrc: Image29, price: "83.99₽/шт" },
+      { name: "M-30", index: 6, size: "165x143x22 мм", imgSrc: Image30, price: "83.99₽/шт" },
+      { name: "M-31", index: 7, size: "165x143x22 мм", imgSrc: Image31, price: "83.99₽/шт" },
+      { name: "M-32", index: 8, size: "165x143x22 мм", imgSrc: Image32, price: "83.99₽/шт" },
+      { name: "M-33", index: 9, size: "165x143x22 мм", imgSrc: Image33, price: "83.99₽/шт" },
+      { name: "M-34", index: 10, size: "165x143x22 мм", imgSrc: Image34, price: "83.99₽/шт" },
+      { name: "M-35", index: 11, size: "165x143x22 мм", imgSrc: Image35, price: "83.99₽/шт" },
+      { name: "M-36", index: 12, size: "165x143x22 мм", imgSrc: Image36, price: "83.99₽/шт" },
+    ],
+  },
+];
+
+export default data;
